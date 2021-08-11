8bf45fc406a4d15e4613958e40056ce4f14b32ef
Carlos <clockwerkz@gmail.com>
2021-08-11 08:22:14 -0700

    working on tic tac toe logic

--- playground/TicTacToe/main.cpp
+++ playground/TicTacToe/main.cpp
@@ -2,23 +2,37 @@
 
 using namespace std;
 
-void PrintSomething();
-void PrintMyName(string name);
-int Square(int number);
+void printBoard(int board[3][3]);
 
 int main() {
-	cout << "The square of 4 is: " << Square(4) << endl;
-	cout << "The square of 5 is: " << Square(5) << endl;
+	int board[3][3] = { {'o','.','.'}, {'o','.','.'}, {'o','.','.'} };
+	printBoard(board);
+	int winConditions[8][3][2] = { { {0,0 }, { 1,0 }, { 2,0 } }, { {0,1 }, { 1,1 }, { 2,1 } } };
+	for (int i = 0; i < sizeof(winConditions) - 1; i++) {
+		char currentChoice = board[winConditions[i][0][0]][winConditions[i][0][1]];
+		bool isWinner = true;
+		for (int j = 0; j < sizeof(winConditions[i]) - 1; j++) {
+			cout << "loop: " << j << endl;
+			int row = winConditions[i][j][0];
+			int col = winConditions[i][j][1];
+			char currentSelection = board[row][col];
+			cout << "Comparing: " << currentChoice << " with " << currentSelection << "." << endl;
+			if (isWinner && currentChoice != currentSelection) {
+				isWinner = false;
+			}
+		}
+		if (isWinner) {
+			cout << "We have a winner!! The " << currentChoice << " player wins!" << endl;
+		}
+	}
 }
 
-int Square(int number) {
-	return number * number;
-}
-
-void PrintSomething() {
-	cout << "Hello from the function!\n";
-}
-
-void PrintMyName(string name) {
-	cout << "Hello, " << name << "! It's nice to meet you.\n";
+void printBoard(int board[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			char currentSelection = board[i][j];
+			cout << currentSelection << (j == 2 ? "" : " | ");
+		}
+		cout << endl << "---------" << endl;
+	}
 }
