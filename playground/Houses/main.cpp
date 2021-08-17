3c09f79467bb8ddd84ccd8376129fc3fac16d943
Carlos <clockwerkz@gmail.com>
2021-08-16 17:50:46 -0700

    basic class example using a House class

---
+++ playground/Houses/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class House {
+	public:
+		void setNumStories(int numStories) {
+			this->numStories = numStories;
+		}
+
+		void setNumWindows(int numWindows) {
+			this->numWindows = numWindows;
+		}
+
+		void setColor(string color) {
+			this->color = color;
+		}
+
+		int getNumStories() const
+		{
+			return this->numStories;
+		}
+		int getNumWindows() const
+		{
+			return this->numWindows;
+		}
+		string getColor() const
+		{
+			return this->color;
+		}
+	private:
+		int numStories;
+		int numWindows;
+		string color;
+};
+
+void printHouseData(House &house);
+
+int main()
+{
+	House myHouse;
+	myHouse.setColor("tan");
+	myHouse.setNumStories(2);
+	myHouse.setNumWindows(6);
+
+	House yourHouse;
+	yourHouse.setColor("dodger blue");
+	yourHouse.setNumStories(3);
+	yourHouse.setNumWindows(10);
+
+	printHouseData(myHouse);
+	printHouseData(yourHouse);
+	return 0;
+}
+
+void printHouseData(House &house) {
+	cout << "This house is " << house.getColor() << " and has "
+		<< house.getNumStories() << " stories and "
+		<< house.getNumWindows() << " windows." << endl;
+}
